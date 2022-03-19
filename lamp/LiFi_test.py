8274dadfebc68d4607ad5018880e6a4fa62ad363
tom1484 <angelus0918866416@gmail.com>
2022-03-20 07:32:34 +0800

    Update

--- lamp/LiFi_test.py
+++ lamp/LiFi_test.py
@@ -4,6 +4,6 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 test = LiFi.LED_IO(17)
 
-test_data = [0, 0, 3, 2, 50, 50, -75, 100, 0, 0, -75, -75, 75, 75, 10, -75, -75, -75, 200, 20]
+test_data = [0, 0, 2, 2, 50, 50, -75, 100, -75, -75, 75, 75, 10, -75, -75, -75, 200, 20]
 while 1:
     test.output(test_data);
